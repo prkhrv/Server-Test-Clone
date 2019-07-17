7017cb7a2d89892bba06e7a73c7a5d8058001cbd
Prakhar Varshney <prakharnegative@gmail.com>
2019-07-17 18:14:39 +0530

    added mongoDB

--- testServer/index.js
+++ testServer/index.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 80;
+var mongoose = require('mongoose');
+
+
+
+mongoose.Promise = global.Promise;
+const dbConfig = require('./config/database.config.js');
+// Connecting to the database 
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useCreateIndex:true,
+    useFindAndModify: false,
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
 
 
 app.get('/',function(req,res){
